c25b3a0e3f7891aaeba1e7e630f2f6f7d044168a
João Julio <joaopjt@gmail.com>
2017-09-30 12:54:51 -0300

    Added migration to usuario table

---
+++ migrations/20170930002824-usuario-migration.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('usuario', { 
+      id: {
+        field: "coduser",
+        type: Sequelize.INTEGER(11),
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        field: 'nome',
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      user: {
+        field: 'login',
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      pass: {
+        field: 'senha',
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      func: {
+        field: 'funcao',
+        type: Sequelize.STRING(200),
+        allowNull: true
+      },
+      privilegy: {
+        field: 'tipo_user',
+        type: Sequelize.STRING(20),
+        allowNull: false
+      },
+      status: {
+        field: 'status',
+        type: Sequelize.STRING(200),
+        allowNull: false
+      },
+      entryDate: {
+        field: 'dt_inclusao',
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        field: 'dt_ult_alteracao',
+        type: Sequelize.DATE(3),
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)'),
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('usuario');
+  }
+};
